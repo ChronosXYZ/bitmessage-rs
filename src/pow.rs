31232e718ad89e87dea2b463e6eb28293edc7325
ChronosX88 <chronosx88@gmail.com>
2023-04-26 07:05:14 +0300

    Implement checking proof of work for object

--- src/pow.rs
+++ src/pow.rs
@@ -2,6 +2,12 @@ use bytes::Bytes;
 use hmac_sha512::Hash;
 use log::info;
 
+#[derive(thiserror::Error, Debug)]
+pub enum PoWError {
+    #[error("proof of work of object is insufficient (trivial_value > target)")]
+    InsufficientProofOfWork,
+}
+
 // Function to do PoW for sending messages into network
 fn do_pow(target: u64, initial_hash: Bytes) -> (u64, u64) {
     info!("PoW has started");
@@ -21,4 +27,19 @@ fn do_pow(target: u64, initial_hash: Bytes) -> (u64, u64) {
 
     info!("PoW has ended");
     return (trial_value, nonce);
+}
+
+fn check_pow(target: u64, nonce: u64, initial_hash: Bytes) -> Result<(), PoWError> {
+    let trial_value = u64::from_be_bytes(
+        Hash::hash(Hash::hash(
+            [&nonce.to_be_bytes()[..], &initial_hash[..]].concat(),
+        ))[0..8]
+            .try_into()
+            .unwrap(),
+    );
+    if trial_value > target {
+        return Err(PoWError::InsufficientProofOfWork);
+    }
+
+    Ok(())
 }

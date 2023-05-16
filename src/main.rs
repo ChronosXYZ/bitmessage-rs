349bfcf206fea6e89ed21ef612be67d382bc3a49
ChronosX88 <chronosx88@gmail.com>
2023-05-16 05:09:43 +0300

    Init UI using Relm4 and GTK+4

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
-use std::error::Error;
-
 use async_std::task;
-use signal_hook::{consts::SIGTERM, iterator::Signals};
+use relm4::{
+    gtk::{
+        self,
+        traits::{BoxExt, GtkWindowExt, OrientableExt},
+    },
+    view, ComponentParts, RelmApp, RelmWidgetExt, SimpleComponent,
+};
 
 mod network;
 mod pow;
 mod repositories;
 
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+struct App {}
+
+#[derive(Debug)]
+enum AppInput {}
+
+#[relm4::component]
+impl SimpleComponent for App {
+    type Input = AppInput;
+    type Output = ();
+    type Init = ();
+
+    view! {
+        gtk::ApplicationWindow {
+            set_default_size: (300, 100),
+
+            set_title = Some("Bitmessage-rs"),
+
+            #[wrap(Some)]
+            set_titlebar = &gtk::HeaderBar {},
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+
+                gtk::Label {
+                    set_label: "Hello, World!",
+                }
+            }
+        }
+    }
+
+    fn init(
+        init: Self::Init,
+        root: &Self::Root,
+        sender: relm4::ComponentSender<Self>,
+    ) -> relm4::ComponentParts<Self> {
+        let model = App {};
+
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+}
+
+fn main() {
     pretty_env_logger::init();
 
     let (mut client, worker) = network::new(None);
 
     task::spawn(worker.run());
 
-    client
-        .start_listening("/ip4/0.0.0.0/tcp/0".parse().unwrap())
-        .await
-        .expect("Listening not to fail");
+    task::block_on(client.start_listening("/ip4/0.0.0.0/tcp/0".parse().unwrap()))
+        .expect("listening not to fail");
 
-    let mut signals = Signals::new(&[SIGTERM])?;
-
-    for sig in signals.forever() {
-        log::debug!("Received signal {:?}", sig);
-        client.shutdown();
-        return Ok(());
-    }
-
-    Ok(())
+    let app = RelmApp::new("io.github.chronosx88.BitmessageRs");
+    app.run::<App>(());
 }
